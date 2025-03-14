26f7bb68a7c6003d1e629d8519899fee9852d5f7
Travis Allen <travis@travisallen.dev>
2025-03-14 17:00:33 -0600

    Set up connection to postgres db with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChainEventModule } from './chain-event/chain-event.module';
+import { ChainEvent } from './chain-event/entities/chain-event.entity';
+import config, { DBConfig } from './config/config';
 
 @Module({
-  imports: [ChainEventModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true,
+    }),
+    ChainEventModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const dbConfig = configService.get('db') as DBConfig;
+        return {
+          type: 'postgres',
+          host: dbConfig.host,
+          port: dbConfig.port,
+          username: dbConfig.username,
+          password: dbConfig.password,
+          database: dbConfig.name,
+          entities: [ChainEvent],
+          synchronize: dbConfig.autoSync,
+        };
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/config.ts
@@ -0,0 +1,56 @@
+interface ReadEnvVariableOptions {
+  required?: boolean;
+  default?: string;
+}
+
+export interface DBConfig {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  name: string;
+  autoSync: boolean;
+}
+
+export interface EtherscanConfig {
+  apiKey: string;
+  baseUrl: string;
+}
+
+class Config {
+  private defaultReadEnvOptions: ReadEnvVariableOptions = {
+    required: false,
+  };
+
+  protected readEnvVariable(
+    key: string,
+    options: ReadEnvVariableOptions = {},
+  ): string {
+    const opts = Object.assign(this.defaultReadEnvOptions, options);
+    const value = process.env[key] || '';
+
+    if (opts.required && !value) {
+      throw new Error(`Environment variable ${key} is required but not set`);
+    }
+
+    return value;
+  }
+
+  protected nodeEnv = this.readEnvVariable('NODE_ENV', { required: true });
+
+  public readonly db: DBConfig = {
+    host: this.readEnvVariable('DB_HOST', { required: true }),
+    port: parseInt(this.readEnvVariable('DB_PORT', { required: true })),
+    username: this.readEnvVariable('DB_USERNAME', { required: true }),
+    password: this.readEnvVariable('DB_PASSWORD', { required: true }),
+    name: this.readEnvVariable('DB_NAME', { required: true }),
+    autoSync: this.nodeEnv === 'development',
+  };
+
+  public readonly etherscan: EtherscanConfig = {
+    apiKey: this.readEnvVariable('ETHERSCAN_API_KEY', { required: true }),
+    baseUrl: this.readEnvVariable('ETHERSCAN_BASE_URL', { required: true }),
+  };
+}
+
+export default () => new Config();
