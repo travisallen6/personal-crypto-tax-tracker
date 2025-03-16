a4b7786579fb54e6c7f4422b9666678b5e45d465
Travis Allen <travis@travisallen.dev>
2025-03-16 01:12:45 -0600

    Add EtherscanService and unit tests for ERC20 transfers
    - Implemented EtherscanService to interact with the Etherscan API for fetching ERC20 token transfers.
    - Created a mock implementation for testing purposes.
    - Added unit tests to validate the functionality of the getErc20Transfers method, including handling of parameters and error scenarios.
    - Ensured proper configuration management using ConfigService for API key and base URL.

---
+++ src/chain-event/etherscan.service.ts
@@ -0,0 +1,78 @@
+import { ConfigService } from '@nestjs/config';
+import { EtherscanConfig } from '../config/config';
+import axios, { AxiosInstance } from 'axios';
+import { Injectable } from '@nestjs/common';
+import { ChainEventTransaction } from './types/chain-event';
+
+interface EtherscanRequest {
+  module: 'account';
+  action: 'tokentx';
+  address: string;
+  startblock: number;
+  endblock?: number;
+  sort: 'asc' | 'desc';
+}
+
+interface EtherscanResponse<T> {
+  status: '1' | '0';
+  message: string;
+  result: T;
+}
+
+@Injectable()
+export class EtherscanService {
+  client: AxiosInstance;
+
+  constructor(configService: ConfigService) {
+    const etherscanConfig =
+      configService.getOrThrow<EtherscanConfig>('etherscan');
+
+    this.buildAxiosClient(etherscanConfig.baseUrl, etherscanConfig.apiKey);
+  }
+
+  private buildAxiosClient(baseUrl: string, apiKey: string) {
+    this.client = axios.create({
+      baseURL: baseUrl,
+    });
+
+    this.client.interceptors.request.use((config) => {
+      const params: Record<string, string> = {
+        ...((config.params as Record<string, string>) || {}),
+        apiKey,
+      };
+      const axiosConfig = Object.assign({}, config, { params });
+
+      return axiosConfig;
+    });
+  }
+
+  private async sendRequest<T>(
+    requestParams: EtherscanRequest,
+  ): Promise<EtherscanResponse<T>> {
+    const result = await this.client.get<EtherscanResponse<T>>('', {
+      params: requestParams,
+    });
+
+    return result.data;
+  }
+
+  public async getErc20Transfers(
+    address: string,
+    startBlock: number,
+    endBlock?: number,
+  ) {
+    const requestParams: EtherscanRequest = {
+      module: 'account',
+      action: 'tokentx',
+      address,
+      startblock: startBlock,
+      sort: 'asc',
+    };
+
+    if (endBlock && endBlock >= 0) {
+      requestParams.endblock = endBlock;
+    }
+
+    return this.sendRequest<ChainEventTransaction[]>(requestParams);
+  }
+}
